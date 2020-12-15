121481eb119a6258bc75083e65c5c6e1ceb0f151
Jérémy Cartier <jerry.c-m@live.fr>
2020-12-15 12:17:51 +0100

    Layout MainActivity

--- app/src/main/java/com/ynov/todolist/MainActivity.java
+++ app/src/main/java/com/ynov/todolist/MainActivity.java
@@ -3,12 +3,30 @@ package com.ynov.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText itemET;
+    private Button btn;
+    private ListView itemsList;
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        itemET = findViewById(R.id.edit_text);
+        btn = findViewById(R.id.add_btn);
+        itemsList = findViewById(R.id.items_list);
+
     }
 }
