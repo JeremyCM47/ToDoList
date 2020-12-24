f9c62dc4eabe5a517c0c459a74f6c7bddba321c8
Jérémy Cartier <jerry.c-m@live.fr>
2020-12-24 12:15:02 +0100

    FileHelper

--- app/src/main/java/com/ynov/todolist/MainActivity.java
+++ app/src/main/java/com/ynov/todolist/MainActivity.java
@@ -3,14 +3,16 @@ package com.ynov.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText itemET;
     private Button btn;
@@ -28,5 +30,22 @@ public class MainActivity extends AppCompatActivity {
         btn = findViewById(R.id.add_btn);
         itemsList = findViewById(R.id.items_list);
 
+        btn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.add_btn:
+                String itemEntered = itemET.getText().toString();
+                adapter.add(itemEntered);
+                itemET.setText("");
+
+
+
+                Toast.makeText(this, "Item Added", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
